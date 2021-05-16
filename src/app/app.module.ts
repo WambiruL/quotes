ded2c8decfa841491ff9eab01bccd36ffee56707
WambiruL <wambirulorraine@gmail.com>
2021-05-16 13:52:56 +0300

    Add the new quote added arguments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { DatePipe } from './date.pipe';
 import { DateCountPipe } from './date-count.pipe';
 import { HighlightDirective } from './highlight.directive';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { QuotesFormComponent } from './quotes-form/quotes-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quotes } from './../quotes';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quotes-form',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesFormComponent implements OnInit {
 
+  newQuote= new Quotes(0,"","","",new Date(),0,0);
+  @Output() add= new EventEmitter<Quotes>()
+  addQuote(){
+    this.add.emit(this.newQuote);
+    this.newQuote=new Quotes(0,"","","",new Date(),0,0);
+  }
   constructor() { }
 
   ngOnInit(): void {
